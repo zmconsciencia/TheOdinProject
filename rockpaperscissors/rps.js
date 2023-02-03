141f4393163d021d6e0212bbfc9e0674fddb6c01
Jose Maria Abecassis Consciencia <jabecass@c2r4s2.42lisboa.com>
2023-02-03 22:07:00 +0000

    not much

--- rockpaperscissors/rps.js
+++ rockpaperscissors/rps.js
@@ -1,3 +1,10 @@
+let ROCK = 0;
+let PAPER = 1;
+let SCISSORS = 2;
+let answer = parseInt(prompt("Enter a number(0 -> ROCK; 1 -> PAPER; 2 -> SCISSORS): "))
+let playerScore = 0;
+let computerScore =0;
+
 function getRandomChoice()
 {
 	let randomNumber = Math.floor(Math.random() * 3)
@@ -11,16 +18,58 @@ function getRandomChoice()
 	}
 }
 
+function turn(playerSelection)
+{
+	if (playerSelection === 0)
+		return("ROCK");
+	else if (playerSelection === 1)
+		return ("PAPER");
+	else if (playerSelection === 2)
+		return ("SCISSORS")
+}
+
 function play(playerSelection, computerSelection)
 {
+	console.log(turn(playerSelection) + ' vs ' + computerSelection )
 	if (playerSelection === ROCK)
 	{
-		if (computerSelection === ROCK)
-			console.log("Draw");
-		else if (computerSelection === PAPER)
-			console.log("Loss");
-		else if (computerSelection === SCISSORS)
-			console.log("Win");
+		if (computerSelection === 'PAPER')
+			return ("Loss")
+		else if (computerSelection === 'ROCK')
+			return ("Draw")
+		else if (computerSelection === 'SCISSORS')
+			return ("Win")
+	}
+	if (playerSelection === PAPER)
+	{
+		if (computerSelection === 'PAPER')
+			return ("Draw")
+		else if (computerSelection === 'ROCK')
+			return ("Win")
+		else if (computerSelection === 'SCISSORS')
+			return ("Loss")
+	}
+	if (playerSelection === SCISSORS)
+	{
+		if (computerSelection === 'PAPER')
+			return ("Win")
+		else if (computerSelection === 'ROCK')
+			return ("Loss")
+		else if (computerSelection === 'SCISSORS')
+			return ("Draw")
 	}
 }
-console.log(play('ROCK', getRandomChoice()))
+
+
+
+function game()
+{
+	let res = play(answer, getRandomChoice())
+	if (res == 'Loss')
+		computerScore++;
+	else if (res == 'Win')
+		playerScore++;
+	return (computerScore + ' vs ' + playerScore)
+}
+console.log(play(answer, getRandomChoice()))
+console.log(game())
